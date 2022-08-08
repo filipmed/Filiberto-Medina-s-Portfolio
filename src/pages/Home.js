9e2a6129d84db0630b1f6b07e4a54ecec6506eb1
filipmed <darkghost595@gmail.com>
2022-08-07 19:24:33 -0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='Home'>
+
+    <div className='about'>
+      <h2>Hello, My name is Filiberto Medina    </h2>
+
+    <div className='skills'></div>
+    </div>
+    
+   
+
+    </div>
+  )
+}
+
+export default Home
